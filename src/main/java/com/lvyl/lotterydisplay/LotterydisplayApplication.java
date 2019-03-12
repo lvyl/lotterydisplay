aa3488334f652e70d83e4b6827ef65f127368b4a
lvyl <15290806520@139.com>
2019-03-12 21:42:03 +0800

    Initial commit

---
+++ src/main/java/com/lvyl/lotterydisplay/LotterydisplayApplication.java
@@ -0,0 +1,13 @@
+package com.lvyl.lotterydisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotterydisplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotterydisplayApplication.class, args);
+    }
+
+}
